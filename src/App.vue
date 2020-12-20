127a7f9ac6f6c77bcd58a4142d5ebb73cfabc36c
pkq322 <1799081863@qq.com>
2020-12-20 17:28:50 +0800

    初始化程序

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 <script>
-import MainTabBar from 'components/content/mainTabBar/MainTabBar'
+import MainTabBar from 'components/content/maintabbar/MainTabBar'
 export default {
   name:'App',
   components:{
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-      <h1>首页</h1>
+  <div id="home">
+    <nav-bar class="nav_bg">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
-</template>
+</template> 
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
 export default {
-    name:'Home'
-}
+  name: "Home",
+  components: {
+    NavBar
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      console.log(res);
+    });
+  }
+};
 </script>
 
 <style scope>
-
+.nav_bg {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
