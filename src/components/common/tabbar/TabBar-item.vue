b84a472fa81fe2fc6a444fa2e141210101919040
pkq322 <1799081863@qq.com>
2020-12-20 15:34:23 +0800

    tabar

---
+++ src/components/common/tabbar/TabBar-item.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="tabbar-item" @click="my_click">
+    <!-- <div>首页</div> -->
+    <!-- 定义2个插槽，分别是图片和文字，因为是需要改动这2个元素的。所以运用插槽 -->
+    <!-- 定义第三个插槽作为2张图片之间的切换，使用v-if来进行显示隐藏，并利用自定义的属性，来切换字体的颜色变动 -->
+    <!-- 所有插槽外都用div进行包裹，可以防止插槽再被替换时，无法显示自己自定义的属性 -->
+    <div v-if="!isActive">
+      <slot name="item-img"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-img-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBaritem",
+  // 父传子，从父组件传入值，更改为string类型
+  props: {
+    path: String,
+    activeColor:{
+      type:String,
+      default:'red'
+    }
+  },
+  data() {
+    return { 
+      // isActive: true
+    };
+  },
+  computed:{
+    isActive(){
+      // 判断获取到的传入的path值与点击处于活跃时的页面的路径完全相等时，为true,则显示该页面的内容
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle(){
+      return this.isActive ? {color:this.activeColor}:{}
+    }
+  },
+  methods: {
+    my_click() {
+      this.$router.push(this.path);
+    }
+  }
+};
+</script>
+
+<style>
+.tabbar-item {
+  flex: 1;
+  height: 49px;
+  text-align: center;
+  font-size: 14px;
+  color: black;
+}
+.tabbar-item img {
+  width: 24px;
+  height: 24px;
+  margin: 3px 0 4px 0;
+  vertical-align: middle;
+}
+/* .active {
+  color: red;
+} */
+</style>
---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <Tab-bar>
+    <!-- path,activeColor作为属性参数传入子组件中 -->
+    <tab-baritem path="/home" activeColor="deeppink">
+      <img slot="item-img" src="assets/img/tabbar/home.png" />
+      <img slot="item-img-active" src="assets/img/tabbar/home_active.png" />
+      <div slot="item-text">首页</div>
+    </tab-baritem>
+    <tab-baritem path="/classifty" activeColor="deeppink">
+      <img slot="item-img" src="assets/img/tabbar/classify.png" />
+      <img slot="item-img-active" src="assets/img/tabbar/classifty_active.png" />
+      <div slot="item-text">分类</div>
+    </tab-baritem>
+    <tab-baritem path="/find" activeColor="deeppink">
+      <img slot="item-img" src="assets/img/tabbar/find.png" />
+      <img slot="item-img-active" src="assets/img/tabbar/find_active.png" />
+      <div slot="item-text">发现</div>
+    </tab-baritem>
+    <tab-baritem path="/mine" activeColor="deeppink">
+      <img slot="item-img" src="assets/img/tabbar/mine.png" />
+      <img slot="item-img-active" src="assets/img/tabbar/mine_active.png" />
+      <div slot="item-text">我的</div>
+    </tab-baritem>
+  </Tab-bar>
+</template>
+
+<script>
+// 导入tabbar组件
+import TabBar from "components/common/tabbar/TabBar";
+import TabBaritem from "components/common/tabbar/TabBar";
+export default {
+  name: "MainTabBar",
+  // 注册组件
+  components: {
+    TabBar,
+    TabBaritem
+  }
+};
+</script>
+
+<style>
+</style>
